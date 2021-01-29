e7d6d70cc4e48e889287e8586eab216569af11f5
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2021-01-29 20:30:38 +0400

    You're a square! - Python

---
+++ 7 KYU/You're a square!/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : You're a square!
+# ID : 54c27a33fb7da0db0100040e
+# Link : https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+# Level : 7 KYU
+# Language : Python
+
+def is_square(n):
+    return n > 0 and (n**0.5).is_integer() or n==0
