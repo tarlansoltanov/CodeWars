868a76eebb414beded3e62be74f8b640d491355b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-19 20:32:52 +0200

    Simple multiplication - Python

---
+++ 8 KYU/Simple multiplication/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : Simple multiplication
+# ID : 583710ccaa6717322c000105
+# Link : https://www.codewars.com/kata/583710ccaa6717322c000105
+# Level : 8 KYU
+# Language : Python
+
+def simple_multiplication(n) :
+    return n * 8 if n % 2 == 0 else n * 9
