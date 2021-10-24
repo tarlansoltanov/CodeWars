c66ae412d3b14390701ac25c0adbb2ebd846dbde
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-24 14:20:16 +0200

    Basic variable assignment - Python

---
+++ 8 KYU/Basic variable assignment/Solution.py
@@ -0,0 +1,10 @@
+# Author : tarlansoltanov
+# Name : Basic variable assignment
+# ID : 50ee6b0bdeab583673000025
+# Link : https://www.codewars.com/kata/50ee6b0bdeab583673000025
+# Level : 8 KYU
+# Language : Python
+
+a = "code"
+b = "wa.rs"
+name = a + b
