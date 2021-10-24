52ffe8b9834b81692f1ea0b2d605e62ec315faa6
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-24 20:16:10 +0200

    Calculating with Functions - Python

---
+++ 5 KYU/Calculating with Functions/Solution.py
@@ -0,0 +1,22 @@
+# Author : tarlansoltanov
+# Name : Calculating with Functions
+# ID : 525f3eda17c7cd9f9e000b39
+# Link : https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39
+# Level : 5 KYU
+# Language : Python
+
+def zero(f=None): return "0" if f == None else eval("0" + f)
+def one(f=None): return "1" if f == None else eval("1" + f)
+def two(f=None): return "2" if f == None else eval("2" + f)
+def three(f=None): return "3" if f == None else eval("3" + f)
+def four(f=None): return "4" if f == None else eval("4" + f)
+def five(f=None): return "5" if f == None else eval("5" + f)
+def six(f=None): return "6" if f == None else eval("6" + f)
+def seven(f=None): return "7" if f == None else eval("7" + f)
+def eight(f=None): return "8" if f == None else eval("8" + f)
+def nine(f=None): return "9" if f == None else eval("9" + f)
+
+def plus(f): return "+" + f
+def minus(f): return "-" + f
+def times(f): return "*" + f
+def divided_by(f): return "//" + f
