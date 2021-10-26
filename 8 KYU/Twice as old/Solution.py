a6ae6f69e839958e2d5d5b763b29bbdf382a13db
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-26 20:00:27 +0200

    Twice as old - Python

---
+++ 8 KYU/Twice as old/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : Twice as old
+# ID : 5b853229cfde412a470000d0
+# Link : https://www.codewars.com/kata/5b853229cfde412a470000d0
+# Level : 8 KYU
+# Language : Python
+
+def twice_as_old(d, s):
+    return abs(d - 2 * s)
