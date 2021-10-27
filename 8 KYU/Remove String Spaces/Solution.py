f966f3bca21acfb09acec8f29bbf477ab34a94e0
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-27 18:21:53 +0200

    Remove String Spaces - Python

---
+++ 8 KYU/Remove String Spaces/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : Remove String Spaces
+# ID : 57eae20f5500ad98e50002c5
+# Link : https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+# Level : 8 KYU
+# Language : Python
+
+def no_space(x):
+    return x.replace(' ', '')
