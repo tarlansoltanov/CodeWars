84697bc7eaec2ca1d43a84fb26874390c4a80633
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-27 21:32:42 +0200

    Odd or Even? - Python

---
+++ 7 KYU/Odd or Even/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : Odd or Even?
+# ID : 5949481f86420f59480000e7
+# Link : https://www.codewars.com/kata/
+# Level : 7 KYU
+# Language : Python
+
+def odd_or_even(arr):
+    return "odd" if sum(arr) % 2 else "even"
