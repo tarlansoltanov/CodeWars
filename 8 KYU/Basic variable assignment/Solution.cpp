1659064055d23377691aa117fcedacf2e21d19dd
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-27 17:02:52 +0200

    Basic variable assignment - C++

---
+++ 8 KYU/Basic variable assignment/Solution.cpp
@@ -0,0 +1,10 @@
+// Author : tarlansoltanov
+// Name : Basic variable assignment
+// ID : 50ee6b0bdeab583673000025
+// Link : https://www.codewars.com/kata/50ee6b0bdeab583673000025
+// Level : 8 KYU
+// Language : C++
+
+std::string a = "code";
+std::string b = "wa.rs";
+std::string name = a + b;
