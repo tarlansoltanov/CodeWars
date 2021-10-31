058bae6a2da7cab91d0764ef7e330d4452f5ebaa
Tarlan Soltanov <tarlansoltanov@outlook.com>
2021-10-31 19:25:06 +0100

    Over The Road - Python

---
+++ 7 KYU/Over The Road/Solution.py
@@ -0,0 +1,9 @@
+# Author : tarlansoltanov
+# Name : Over The Road
+# ID : 5f0ed36164f2bc00283aed07
+# Link : https://www.codewars.com/kata/5f0ed36164f2bc00283aed07
+# Level : 7 KYU
+# Language : Python
+
+def over_the_road(a, n):
+    return (n - (a // 2)) * 2 + int(a % 2 == 0) 
